ea98f68925583f9f12cb6ef637e326307e761109
yuanchen <yuanchen1103@gmail.com>
2020-04-19 03:08:24 +0800

    add mask info block

--- components/TaiwanIsHelping/index.jsx
+++ components/TaiwanIsHelping/index.jsx
@@ -26,9 +26,21 @@ const TaiwanIsHelping = () => {
     <section className={styles.section}>
       <Container>
         <Row>
-          <Col>
+          <Col md={8}>
             <h1 className={styles.title}>TAIWAN IS HELPING</h1>
             <h2 className={styles.subTitle}>臺灣不只可以幫忙，而且正在幫忙</h2>
+          </Col>
+          <Col md={4}>
+            <div className={styles.infoWrapper}>
+              <div className={styles.infoGroup}>
+                <h6>捐贈口罩數</h6>
+                <p>{thousandComma(1000)}</p>
+              </div>
+              <div className={styles.infoGroup}>
+                <h6>捐贈國家數</h6>
+                <p>{thousandComma(32)}</p>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
@@ -48,12 +60,12 @@ const TaiwanIsHelping = () => {
               ));
             }}
           </Geographies>
-          {markers.map(({
-            coordinates,
-            name,
-            count,
-          }) => (
-            <Tooltip key={name} placement="top" overlay={`${name}: ${thousandComma(count)}`}>
+          {markers.map(({ coordinates, name, count }) => (
+            <Tooltip
+              key={name}
+              placement="top"
+              overlay={`${name}: ${thousandComma(count)}`}
+            >
               <Marker coordinates={coordinates} className={styles.marker}>
                 <g
                   fill="none"
@@ -61,7 +73,10 @@ const TaiwanIsHelping = () => {
                   strokeLinejoin="round"
                   transform="translate(-12, -24)"
                 >
-                  <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" fill="#0ccda3" />
+                  <path
+                    d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z"
+                    fill="#0ccda3"
+                  />
                   <circle cx="12" cy="10" r="3" fill="#ffffff" />
                 </g>
               </Marker>
