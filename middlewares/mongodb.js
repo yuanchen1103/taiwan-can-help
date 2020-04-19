11c4c390b1d6f5433ef2885f3d745b3012ed8829
Evis Cheng <e920528@gmail.com>
2020-04-19 20:46:12 +0800

    [New Feature] connect mongo atlas

---
+++ middlewares/mongodb.js
@@ -0,0 +1,15 @@
+import { MongoClient } from 'mongodb';
+
+const url = 'mongodb+srv://user:hBQfDUDqaccBYRSQ@cluster0-ghmml.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export default async function database(req, res, next) {
+  if (!client.isConnected()) await client.connect();
+  req.dbClient = client;
+  req.db = client.db(process.env.NODE_ENV === 'production' ? 'production' : 'test');
+  console.log('mongodb connected');
+  return next();
+}
