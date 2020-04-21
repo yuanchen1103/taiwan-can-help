c0f64d3117cbb068ea32ce7663c1c1dde3a1cbac
yuanchen <yuanchen1103@gmail.com>
2020-04-22 00:58:45 +0800

    add banner section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { I18nProvider } from '@/i18n/I18nContext';
 import Header from '@/components/Header';
-// import Example from '@/components/Example';
 import TaiwanIsHelping from '@/components/TaiwanIsHelping';
 import YouCanHelp from '@/components/YouCanHelp';
 import Footer from '@/components/Footer';
+import Banner from '@/components/Banner';
 
 export default function Index() {
   return (
     <I18nProvider>
-      <div>
-        <Header />
-        {/* <Example /> */}
-        <TaiwanIsHelping />
-        <YouCanHelp />
-        <Footer />
-      </div>
+      <Banner />
+      <Header />
+      <TaiwanIsHelping />
+      <YouCanHelp />
+      <Footer />
     </I18nProvider>
   );
 }
