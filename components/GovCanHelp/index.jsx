0ce54dd883afbb422b4f8854330566a214f03b55
jherkyesse <>
2020-04-22 16:49:15 +0800

    featurs: add react-flip-move shuffle mediaAssetList

--- components/GovCanHelp/index.jsx
+++ components/GovCanHelp/index.jsx
@@ -1,5 +1,12 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, {
+  useContext,
+  useState,
+  useEffect,
+  forwardRef,
+} from 'react';
+import PropTypes from 'prop-types';
 import { debounce } from 'lodash';
+import FlipMove from 'react-flip-move';
 import Swiper from 'react-id-swiper';
 import { I18nContext } from '@/i18n/I18nContext';
 import styles from './GovCanHelp.scss';
@@ -83,21 +90,76 @@ const Carousel = () => {
   );
 };
 
-const Blockquote = () => {
-  const blockquoteList = [
-    '2020/01/30 發現武漢肺炎，政府開始動起來。',
-    '2020/01/30 發現武漢肺炎，政府開始動起來。',
-    '2020/01/30 發現武漢肺炎，政府開始動起來。',
-    '2020/01/30 發現武漢肺炎，政府開始動起來。2020/01/30 發現',
+const MediaAsset = forwardRef(({ mediaAsset }, ref) => (
+  <div ref={ref} className={`d-flex ${styles.mediaAsset}`}>
+    <div>
+      {mediaAsset.assetDate}
+      |
+      {mediaAsset.assetOrganization}
+      {mediaAsset.assetContent}
+    </div>
+  </div>
+));
+
+MediaAsset.propTypes = {
+  mediaAsset: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+
+const MediaAssetList = () => {
+  const mockList = [
+    {
+      assetContent: '成為口罩國家隊，提供工具機專業技術，使得我國口罩產',
+      assetDate: '2020/03/28',
+      assetOrganization: '紐約時報1',
+      assetLink: 'https://taiwancanhelp.us/',
+    },
+    {
+      assetContent: '成為口罩國家隊，提供工具機專業技術，使得我國口',
+      assetDate: '2020/03/28',
+      assetOrganization: '紐約時報2',
+      assetLink: 'https://taiwancanhelp.us/',
+    },
+    {
+      assetContent: '成為口罩國家隊，提供工具機專業技術，使得我國口罩產量加速成長',
+      assetDate: '2020/03/28',
+      assetOrganization: '紐約時報3',
+      assetLink: 'https://taiwancanhelp.us/',
+    },
+    {
+      assetContent: '成為口罩國家隊，提供工具機專業技術，使得我國口罩產量加速成長',
+      assetDate: '2020/03/28',
+      assetOrganization: '紐約時報4',
+      assetLink: 'https://taiwancanhelp.us/',
+    },
+    {
+      assetContent: '成為口罩國家隊，提供工具機專業技術，使得我國口罩產量加速成長',
+      assetDate: '2020/03/28',
+      assetOrganization: '紐約時報5',
+      assetLink: 'https://taiwancanhelp.us/',
+    },
+    {
+      assetContent: '成為口罩國家隊，提供工具機專業技術，使得我國口罩產量加速成長',
+      assetDate: '2020/03/28',
+      assetOrganization: '紐約時報6',
+      assetLink: 'https://taiwancanhelp.us/',
+    },
   ];
+  const [mediaAssetList, setMediaAssetList] = useState(mockList);
+  useEffect(() => {
+    setInterval(() => {
+      mediaAssetList.push(mediaAssetList.splice(0, 1)[0]);
+      setMediaAssetList([...mediaAssetList]);
+    }, 3000);
+  }, []);
   return (
     <div className="container">
-      <div className="row">
-        {blockquoteList.map((blockquote, index) => (
-          <div key={+index} className={`d-flex ${styles.blockquote}`}>
-            <div>{blockquote}</div>
-          </div>
-        ))}
+      <div className={`d-flex align-items-center justify-content-center ${styles.mediaAssetContainer}`}>
+        <FlipMove className={styles.width100}>
+          {mediaAssetList.map((mediaAsset, index) => index < 4 && (
+            <MediaAsset key={mediaAsset.assetOrganization} mediaAsset={mediaAsset} />
+          ))}
+        </FlipMove>
       </div>
     </div>
   );
@@ -133,7 +195,7 @@ const GovCanHelp = () => {
         </div>
       </section>
       <section className={styles.section}>
-        <Blockquote />
+        <MediaAssetList />
       </section>
     </>
   );
